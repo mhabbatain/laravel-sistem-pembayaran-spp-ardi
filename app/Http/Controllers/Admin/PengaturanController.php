5c60545713fb27ad1a9baf1eface83b015e361dd
mhabbatein <habiburrahmanmuhammad95@gmail.com>
2025-12-30 11:01:30 +0700

    fix pengaturan

--- app/Http/Controllers/Admin/PengaturanController.php
+++ app/Http/Controllers/Admin/PengaturanController.php
@@ -13,4 +13,15 @@ class PengaturanController extends Controller
         $pengaturan = Pengaturan::all();
         return view('admin.pengaturan.index', compact('pengaturan'));
     }
+
+    public function update(Request $request)
+    {
+        $data = $request->except('_token');
+
+        foreach ($data as $key => $value) {
+            Pengaturan::where('key', $key)->update(['value' => $value]);
+        }
+
+        return redirect()->route('admin.pengaturan.index')->with('success', 'Pengaturan berhasil diperbarui');
+    }
 }
